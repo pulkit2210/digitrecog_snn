ac0a9378a56a2c72bf63d91eaea432f3dd28565a
Pulkit Katdare <radiantpulkit@gmail.com>
2016-04-28 09:25:52 +0530

    added the connections tutorial

--- helloworld.py
+++ helloworld.py
@@ -20,6 +20,7 @@ ylabel('Membrane potential (in mV)')
 title('Membrane potential for neuron 0')
 show()
 '''
+'''
 tau = 20 * msecond        # membrane time constant
 Vt = -50 * mvolt          # spike threshold
 Vr = -60 * mvolt          # reset value
@@ -35,4 +36,32 @@ G.V = Vr + rand(40) * (Vt - Vr)
 run(1 * second)
 print M.nspikes
 raster_plot()
+show()
+'''
+tau_a = 1 * ms 
+tau_b = 10 * ms
+v_t = 10*mV
+V_r = 0 * mV
+eqs = Equations('''
+      dVa/dt = -Va/tau_a : volt
+      dVb/dt = -Vb/tau_b : volt
+      ''')
+
+spiketimes = [(0,1 * ms) , (0,4 * ms) ,
+              (1,2 * ms) , (1,3 * ms)]
+#G = SpikeGeneratorGroup(2,spiketimes)
+G1 = SpikeGeneratorGroup(2, spiketimes)
+G2 = NeuronGroup(N=1, model=eqs, threshold=v_t, reset=V_r)
+#C = Connection(G,H,state)
+C1 = Connection(G1, G2, 'Va')
+C2 = Connection(G1, G2, 'Vb')
+C1[0, 0] = 6 * mV
+C2[1, 0] = 3 * mV
+Ma = StateMonitor(G2, 'Va', record=True)
+Mb = StateMonitor(G2, 'Vb', record=True)
+
+run(10 * ms)
+
+plot(Ma.times, Ma[0])
+plot(Mb.times, Mb[0])
 show()
